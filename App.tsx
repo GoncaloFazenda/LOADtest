b3cab11681aa91d68ea366cfac704285b1af352a
Gonçalo Fazenda <goncalo.fazenda@logirix.com>
2023-11-28 23:42:02 +0000

    UI: Installing React Native Paper & vector Icons

--- App.tsx
+++ App.tsx
@@ -4,16 +4,22 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import FeedController from './src/presentation/movie/containers/FeedController';
 import MovieDetailsController from './src/presentation/movie/containers/MovieDetailsController';
+import {PaperProvider} from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={FeedController} />
-        <Stack.Screen name="Movie Details" component={MovieDetailsController} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={FeedController} />
+          <Stack.Screen
+            name="Movie Details"
+            component={MovieDetailsController}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- src/presentation/movie/containers/FeedScreen.tsx
+++ src/presentation/movie/containers/FeedScreen.tsx
@@ -1,6 +1,7 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
 import generalStyles from '../../general/generalStyles';
+import {Button} from 'react-native-paper';
 
 type Props = {
   onPressViewDetails: () => void;
@@ -9,7 +10,9 @@ type Props = {
 export default function FeedScreen(props: Props) {
   return (
     <View style={generalStyles.screen}>
-      <Button title="View Details" onPress={props.onPressViewDetails} />
+      <Button mode="elevated" onPress={props.onPressViewDetails}>
+        View Details
+      </Button>
     </View>
   );
 }
